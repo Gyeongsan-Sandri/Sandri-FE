5b87e5c281532c7ed30bd21c41e7ed4dea5982f1
unknown <22321627@yu.ac.kr>
2025-11-18 12:15:55 +0900

    파일 변경

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import MainLayout from "./layouts/MainLayout";
 import RequireAuth from "./components/RequireAuth";
 import LoadingPage from "./pages/others/loading/loading";
 
-// Pages - Lazy Loading
 const Home = lazy(() => import("./pages/main/home/home"));
 const Magazine = lazy(() => import("./pages/main/magazine/magazine"));
 const SpotSearch = lazy(() => import("./pages/main/spot_search/spot_search"));
@@ -16,13 +15,16 @@ const Collection = lazy(() => import("./pages/collection/collection_start"));
 const TourSpotDetail = lazy(() => import("./pages/tourspots/tourspots/tourspots"));
 const TourSpotsReview = lazy(() => import("./pages/tourspots/tourspots_review/tourspots_review"));
 
-const MyRoute = lazy(() => import("./pages/route/myroute/myroute"));
+const RouteList = lazy(() => import("./pages/route/route_list/route_list"))
+const TravelRoute = lazy(() => import("./pages/route/route/route"));
 const RouteCategory = lazy(() => import("./pages/route/route_category/route_category"));
 const RouteSearch = lazy(() => import("./pages/route/route_search/route_search"));
-const RouteModify = lazy(() => import("./pages/route/route_modify/route_modify"));
+
+const Likes = lazy(() => import("./pages/likes/likes"))
 
 const MyPage = lazy(() => import("./pages/mypage/mypage/mypage"));
 const MyPageModify = lazy(() => import("./pages/mypage/mypage_modify/mypage_modify"));
+const MyPageReview = lazy(() => import("./pages/mypage/mypage_review/mypage_review"));
 const MyPageTravelList = lazy(() => import("./pages/mypage/mypage_travel_list/mypage_travel_list"));
 const Login = lazy(() => import("./pages/auth/login/login"));
 const Signup = lazy(() => import("./pages/auth/signup/signup"));
@@ -39,11 +41,11 @@ export default function App() {
             {/* 홈  */}
             <Route index element={<Home />} />
             <Route path="magazine" element={<Magazine />} />
-            <Route path="api/search" element={<SpotSearch />} />
-            <Route path="api/categories" element={<SpotCategory />} />
+            <Route path="spot/search" element={<SpotSearch />} />
+            <Route path="spot/categories" element={<SpotCategory />} />
 
             {/* 관광지 */}
-            <Route path="places/tourspots/:id" element={<TourSpotDetail />} />
+            <Route path="places/:placeId" element={<TourSpotDetail />} />
             <Route path="places/:placeId/reviews" element={<TourSpotsReview />} />
 
             {/* 루트 */}
@@ -51,22 +53,24 @@ export default function App() {
             <Route path="routes/search" element={<RouteSearch />} />
 
             {/* 로그인 필요한 페이지 */}
-            {/* <Route element={<RequireAuth />}> */}
-              <Route path="routes/:routeId" element={<MyRoute />} />
-              <Route path="routes/:routeId/modify" element={<RouteModify />} />
+            <Route element={<RequireAuth />}>
+              <Route path="routes/list" element={<RouteList />} />
+              <Route path="routes/:routeId" element={<TravelRoute />} />
+
               <Route path="mypage" element={<MyPage />} />
-              <Route path="mypage/reviews" element={<MyPage />} />
+              <Route path="mypage/modify" element={<MyPageModify />} />
+              <Route path="mypage/reviews" element={<MyPageReview />} />
               <Route path="mypage/travel-list" element={<MyPageTravelList />} />
+
               <Route path="collection" element={<Collection />} />
-            {/* </Route> */}
+
+              <Route path="likes" element={<Likes />} />
+            </Route>
           </Route>
 
           {/* 인증 페이지  */}
           <Route path="/users/login" element={<Login />} />
           <Route path="/users/register" element={<Signup />} />
-          
-          {/* 마이페이지 수정 */}
-          <Route path="/mypage/modify" element={<MyPageModify />} />
           
           {/* 테스트 페이지 */}
           <Route path="/test" element={<Test />} />
